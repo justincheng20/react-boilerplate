0ebafd86e6e95439f3af96bf027e73227f78c9e5
justincheng20 <justincheng20@gmail.com>
2020-05-12 11:09:46 -0700

    Add basic form

--- app/containers/NewPage/index.js
+++ app/containers/NewPage/index.js
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
+import PropTypes from 'prop-types';
 // import { FormattedMessage } from 'react-intl';
 // import messages from './messages';
 
 
-export default function NewPage() {
+export default function NewPage({ item, onSubmitForm, onChangeItem }) {
+
+  const [formData, setFormData] = useState({
+    item: "",
+  });
+
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    console.log("Check out state ->", formData);
+    // do something with the data submitted
+  };
+
+  const handleChange = evt => {
+    const { name, value } = evt.target;
+    setFormData(fData => ({
+      ...fData,
+      [name]: value
+    }));
+  };
+
   return (
     <div>
       <h1>
         This is a test.
     </h1>
-      <form >
-        <label htmlFor="item">
-          <input
-            id="item"
-            type="text"
-            placeholder=""
-          />
-        </label>
-      </form>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="item">Item:</label>
+      <input
+        id="item"
+        name="item"
+        value={formData.item}
+        onChange={handleChange}
+      />
+      <button>Add a new item!</button>
+    </form>
     </div>
   );
 }
+
+NewPage.propTypes = {
+  onSubmitForm: PropTypes.func,
+  item: PropTypes.string,
+  onChangeItem: PropTypes.func,
+};
