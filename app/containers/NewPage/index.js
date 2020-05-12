e8291c281eda441178b6f2cb3adc0a414a8a42f2
justincheng20 <justincheng20@gmail.com>
2020-05-12 10:36:35 -0700

    render non-functional form

--- app/containers/NewPage/index.js
+++ app/containers/NewPage/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 // import { FormattedMessage } from 'react-intl';
 // import messages from './messages';
 
+
 export default function NewPage() {
   return (
-    <h1>
-      This is a test.
+    <div>
+      <h1>
+        This is a test.
     </h1>
+      <form >
+        <label htmlFor="item">
+          <input
+            id="item"
+            type="text"
+            placeholder=""
+          />
+        </label>
+      </form>
+    </div>
   );
 }
