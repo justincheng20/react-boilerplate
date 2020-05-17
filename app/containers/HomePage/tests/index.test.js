097ee309ab4c4c157e42b2f4d25de11bf26072bf
justincheng20 <justincheng20@gmail.com>
2020-05-17 13:58:28 -0700

    Add home mapDispatchtoProps test

--- app/containers/HomePage/tests/index.test.js
+++ app/containers/HomePage/tests/index.test.js
@@ -8,7 +8,8 @@ import { IntlProvider } from 'react-intl';
 import { Provider } from 'react-redux';
 import { browserHistory, BrowserRouter } from 'react-router-dom';
 
-import { HomePage } from '../index';
+import { HomePage, mapDispatchToProps } from '../index';
+import { loadItems } from '../../App/actions';
 import configureStore from '../../../configureStore';
 
 describe('<HomePage />', () => {
@@ -31,5 +32,22 @@ describe('<HomePage />', () => {
       </Provider>,
     );
     expect(firstChild).toMatchSnapshot();
+  });
+
+  describe('mapDispatchToProps', () => {
+    describe('onLoad', () => {
+      it('should be injected', () => {
+        const dispatch = jest.fn();
+        const result = mapDispatchToProps(dispatch);
+        expect(result.onLoad).toBeDefined();
+      });
+
+      it('should dispatch loadItems when called', () => {
+        const dispatch = jest.fn();
+        const result = mapDispatchToProps(dispatch);
+        result.onLoad();
+        expect(dispatch).toHaveBeenCalledWith(loadItems());
+      });
+    });
   });
 });
